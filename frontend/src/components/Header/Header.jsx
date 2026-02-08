339dc352e6611a5a612fa12febf537fd563b577e
Shubham Vaishnav <159063010+Shubhamvaishnav18@users.noreply.github.com>
2026-02-08 18:58:54 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,58 +1,20 @@
-import React from "react";
-import Slider from "react-slick";
-import "./Header.css";
+import React from "react"
+import "./Header.css"
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+
   const navigate = useNavigate();
-
-  const images = [
-    "/header_1.png",
-    "/header_img.png",
-    "/header_1.png"
-  ];
-
-  // Slider settings
-  const settings = {
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    speed: 800,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    fade: false,
-    cssEase: "linear",
-  };
 
   return (
     <div className="header">
-
-      <Slider {...settings} className="header-slider">
-        {images.map((img, i) => (
-          <div key={i}>
-            <div
-              className="header-slide"
-              style={{ backgroundImage: `url(${img})` }}
-            ></div>
-          </div>
-        ))}
-      </Slider>
-
-      {/* Your original content remains the same */}
-      {/* <div className="header-contents">
-        <h2>Order your favourite food here</h2>
-        <p>
-          Choose from a diverse menu featuring a delectable array of dishes crafted
-          with the finest ingredients and culinary expertise. 
-        </p>
-        <button onClick={() => navigate("/menu")}>View Menu</button>
-      </div> */}
-
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button onClick={() => navigate('/menu')}>View Menu</button>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
