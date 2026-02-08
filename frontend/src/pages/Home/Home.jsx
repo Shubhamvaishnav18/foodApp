489c2d974846c88fad5d313e19b3b2a718243d05
Shubham Vaishnav <159063010+Shubhamvaishnav18@users.noreply.github.com>
2026-02-08 18:56:07 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -7,40 +7,10 @@ import AppDownload from "../../components/AppDownload/AppDownload"
 
 const Home = () => {
 
-    const [category, setCategory] = useState("All");
-
-    const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedLocation, setSelectedLocation] = useState("");
-
-  const locations = ["TB6", "UCICI", "SC", "BH4", "Girls Hostel"];
+   const [category, setCategory] = useState("All");
 
   return (
     <div>
-        <div className="header-search">
-        <input
-          type="text"
-          placeholder="Search Location..."
-          value={selectedLocation}
-          onClick={() => setShowDropdown(!showDropdown)}
-          readOnly
-        />
-
-        {showDropdown && (
-          <ul className="search-dropdown">
-            {locations.map((loc, i) => (
-              <div className="li"
-                key={i}
-                onClick={() => {
-                  setSelectedLocation(loc);
-                  setShowDropdown(false);
-                }}
-              >
-                {loc}
-              </div>
-            ))}
-          </ul>
-        )}
-      </div>
         <Header />
         <ExploreMenu category={category} setCategory={setCategory} />
         <FoodDisplay category={category} />
